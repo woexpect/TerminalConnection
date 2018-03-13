dfa18255a2a76cfe038f1a267fdc3085005d451e
unknown <2092063@labinfo.is.escuelaing.edu.co>
2018-03-13 12:35:11 -0500

    tipos de conexiones funcionando

--- src/co/com/pgr/Main.java
+++ src/co/com/pgr/Main.java
@@ -1,51 +1,81 @@
 package co.com.pgr;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Properties;
+import java.util.Scanner;
 
 import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 
 public class Main {
 	
 	public static String user = "2092063";
-	public static String password = "";
+	public static String password = "Simpsons741";
 	public static String host = "10.2.67.116";
+	public static String commandInput = "";
 	public static int port = 22;
 	public static JSch jsch = null;
 	public static Session session = null;
 	public static ChannelExec channel = null;
 	public static BufferedReader in = null;
+	public static Scanner javaInput = null;
+	public static Properties config = null; 
+	
 
 	public static void main(String[] args) {
 		
+		javaInput = new Scanner(System.in);
+//		getUserCredentials();
 		createConnection();
 		menuInteraction();
 		closeConnection();
 		
 	}
 
+	private static void getUserCredentials() {
+		
+		System.out.println("Please, enter your user name:");
+		user = javaInput.nextLine();
+		System.out.println("Please, enter your password for user name [" + user + "] :");
+		password = javaInput.nextLine();		
+	}
+
 	private static void menuInteraction() {
 		
 		try {
-			String msg=null;
-			while((msg=in.readLine())!=null){
-			  System.out.println(msg);
-			}			
+			
+			String msg = null;
+			System.out.println(user + "@java: ");
+			commandInput = javaInput.nextLine();
+			
+			while(commandInput != null || !commandInput.equals("")) {
+				
+				channel.setCommand(commandInput);
+				channel.connect();
+				
+				while((msg = in.readLine()) != null){
+					System.out.println(msg + "a");
+				}
+				if (!session.isConnected()) {
+					System.out.println("Session dwon.");
+					commandInput = "";
+		        } else {
+		        	System.out.println(user + "@java: ");
+		        	commandInput = javaInput.nextLine();
+		        }
+				
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
+			closeConnection();
 		}
 		
 	}
 
 	private static void closeConnection() {
-		// TODO Auto-generated method stub
 		channel.disconnect();
 		session.disconnect();
 	}
@@ -59,7 +89,7 @@ public class Main {
 			session = jsch.getSession(user, host, 22);
 			session.setPassword(password);
 			//Setting Connection properties
-			Properties config = new Properties();
+			config = new Properties();
 			config.put("StrictHostKeyChecking", "no");
 			session.setConfig(config);
 			//Create connection to remote server
@@ -72,8 +102,7 @@ public class Main {
 			System.out.println("Channel open.");
 			//Set the input reader of channel conection. 
 			in = new BufferedReader(new InputStreamReader(channel.getInputStream()));
-			channel.setCommand("ls");
-			channel.connect();
+			System.out.println("Welcome, " + user + ".");
 		} catch (Exception e) {
 			System.out.println("An error ocurred while creating connection.");
 			e.printStackTrace();
